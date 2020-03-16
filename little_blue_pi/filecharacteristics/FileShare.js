8bc7769a6e0d0bfb8b3af5961931d433d433e90c
Els1029 <56343212+Els1029@users.noreply.github.com>
2020-03-16 23:11:43 +0000

    made the mainfest notify to send larger mainfests

--- little_blue_pi/filecharacteristics/FileShare.js
+++ little_blue_pi/filecharacteristics/FileShare.js
@@ -21,24 +21,6 @@ var file = null;
 
 FileShareCharacteristic.prototype.onReadRequest = function(offset, callback) {
 
-  // if(!offset) {
-
-  //   var loadAverage = os.loadavg().map(function(currentValue, index, array){
-
-  //     return currentValue.toFixed(3);
-  //   });
-
-  //   this._value = new Buffer(JSON.stringify({
-  //     'oneMin' : loadAverage[0],
-  //     'fiveMin': loadAverage[1],
-  //     'fifteenMin': loadAverage[2]
-  //   }));
-  // }
-
-  // console.log('LoadAverageCharacteristic - onReadRequest: value = ' +
-  //   this._value.slice(offset, offset + bleno.mtu).toString()
-  // );
-
   callback(this.RESULT_SUCCESS, this._value);
 };
 
@@ -52,16 +34,13 @@ FileShareCharacteristic.prototype.onWriteRequest = function(data, offset, withou
   {
     if(fs_offset < file.length)
     {
-      //callback(this.RESULT_SUCCESS);
       console.log(file.slice(fs_offset,fs_offset + bleno.mtu).toString());
-      //wait(100);
       this._value = new Buffer(file.slice(fs_offset,fs_offset + bleno.mtu).toString());
       this._updateValueCallback(this._value);
       fs_offset += bleno.mtu;
       callback(this.RESULT_SUCCESS);
     }
     else{
-
       this._updateValueCallback(new Buffer("DONE"));
     }
   }
@@ -80,7 +59,6 @@ FileShareCharacteristic.prototype.onSubscribe = function(maxValueSize, updateVal
   console.log("FileShareCharacteristic - onSubscribe");
   console.log('Began sending File');
   this._updateValueCallback = updateValueCallback;
-  //sendFile();
 };
 
 
@@ -90,9 +68,9 @@ FileShareCharacteristic.prototype.onUnsubscribe = function() {
   this._updateValueCallback = null;
 };
 
-function setFile(filename){
+function setFile(path){
   return new Promise(function(resolve, reject){
-    fs.readFile('bluetooth_pi_2/little_blue_pi/files/test.csv','utf8', function (err, data) {
+    fs.readFile(path,'utf8', function (err, data) {
       if (err) {
           reject(err);
       }
@@ -103,32 +81,6 @@ function setFile(filename){
     });
   });
 }
-
-function wait(ms)
-{
-var d = new Date();
-var d2 = null;
-do { d2 = new Date(); }
-while(d2-d < ms);
-}
-// function sendFile(){
-//   fs.readFile('Project/bluetooth_pi_2-master/little_blue_pi/filecharacteristics/test.csv','utf8', function (err, data) {
-//     if (err) {
-//         throw err;
-//     }
-//     const content = data;
-//     var offset = 0;
-
-//     while(offset<data.length)
-//     {
-//       this._updateValueCallback(content.slice(offset,offset + bleno.mtu));
-//       offset += bleno.mtu;
-//     }
-//     // Invoke the next step here however you like
-//     //console.log(content);
-//     this._updateValueCallback("DONE");   // Put all of the code here (not the best solution)
-//   });
-// };
 
 util.inherits(FileShareCharacteristic, BlenoCharacteristic);
 module.exports = FileShareCharacteristic;
--- little_blue_pi/filecharacteristics/getManifest.js
+++ little_blue_pi/filecharacteristics/getManifest.js
@@ -9,11 +9,12 @@ var BlenoCharacteristic = bleno.Characteristic;
 var getManifestCharacteristic = function() {
   getManifestCharacteristic.super_.call(this, {
     uuid: '19cbb8e3-a1c5-4b9a-b98c-3ba0f3b10dc3',
-    properties: ['read'],
+    properties: ['read', 'write', 'notify'],
     value: null
   });
 
- this._value = new Buffer(0);
+  this._value = new Buffer(0);
+  this._updateValueCallback = null;
 };
 
 var fs_offset =0;
@@ -30,6 +31,51 @@ getManifestCharacteristic.prototype.onReadRequest = function(offset, callback) {
   }
   callback(this.RESULT_SUCCESS, this._value.slice(offset, this._value.length));
 };
+
+
+
+getManifestCharacteristic.prototype.onWriteRequest = function(data, offset, withoutResponse, callback)
+{
+  if(!offset)
+	{
+  }
+  var stringData = data.toString('ascii');
+  if(stringData == "CONTINUE")
+  {
+    if(fs_offset < this._value.length)
+    {
+      console.log(this._value.slice(fs_offset,fs_offset + bleno.mtu).toString());
+      //this._value = new Buffer(file.slice(fs_offset,fs_offset + bleno.mtu).toString());
+      this._updateValueCallback(this._value.slice(fs_offset,fs_offset + bleno.mtu).toString());
+      fs_offset += bleno.mtu;
+      callback(this.RESULT_SUCCESS);
+    }
+    else{
+      this._updateValueCallback(new Buffer("DONE"));
+    }
+  }
+  else
+  {
+    var tree = dirTree("/some/path");
+    console.log(JSON.stringify(tree));
+    this._value = new Buffer(JSON.stringify(tree));
+    callback(this.RESULT_SUCCESS);
+  }
+};
+
+getManifestCharacteristic.prototype.onSubscribe = function(maxValueSize, updateValueCallback) {
+  console.log("getManifestCharacteristic - onSubscribe");
+  //console.log('Began sending File');
+  this._updateValueCallback = updateValueCallback;
+};
+
+
+getManifestCharacteristic.prototype.onUnsubscribe = function() {
+  console.log('getManifestCharacteristic - onUnsubscribe');
+
+  this._updateValueCallback = null;
+};
+
 
 util.inherits(getManifestCharacteristic, BlenoCharacteristic);
 module.exports = getManifestCharacteristic;
